a64adcd582b306861088a7c87534c0dd572c5588
Blini73 <blinmehmeti10@gmail.com>
2025-03-05 17:45:07 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html   <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ds-theme</title>
+</head>
+<body <?php body_class(); ?>>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, non.</p>
+    <div id="page" class="site">
+        <header>
+     <section class="top-bar">
+      <div class="logo">
+        Logo
+</div>
+<div id="search-box">
+    Search
+</div>
+     </section>
+     <section class="menu-area">
+        <nav class="main-menu">
+            Menu
+        </nav>
+     </section>
+       </header>
+       <!-- header -->
+        <div id="content" class="site-content">
+        <div id="primary" class="content-area">
+        <main id="main" class="site-main">
+            <section class="hero">
+             Hero
+            </section>
+            <section class="services">
+             Services
+            </section>
+            <section class="home-blog">
+             Blog
+            </section>
+        </div>
+        </div>
+        </main>
+        <!--body-->
+        <footer class="site-footer">
+           Footer
+        </footer>
+</body>
+</html>
+<!--footer-->
